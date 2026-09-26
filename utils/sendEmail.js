ab129ca8dc4812d1641aaa82093c0ce839b824f8
Heritage777 <heritageabajesude@gmail.com>
2026-09-26 13:16:03 +0100

    Replace Gmail SMTP with Resend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,34 +1,27 @@
-const nodemailer = require('nodemailer')
+const { Resend } = require('resend')
 const otpEmailTemplate = require('./otpEmailTemplate')
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 const sendEmail = async (email, title, body) => {
     try {
-        //we must first create a transporter
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST, //this means through whom are we sending this mail
-            port: process.env.MAIL_PORT, //this is the port number of the host
-            secure: false, //this means we are not using a secure connection. If we were using a secure connection, we would set this to true.
-            auth: {
-                user: process.env.MAIL_USER, //who is sending the mail? in this case, me.
-                pass: process.env.MAIL_PASSWORD // password/credential for my sending account
-            }
-        })
 
-        // to send the mail
-        const info = await transporter.sendMail({
-            from: {
-                name: 'Heritage Blog App',
-                address: process.env.MAIL_USER
-            }, //Not authenticate. It is simply what the recipient sees as sender.
-            to: `${email}`,
-            subject: `${title}`,
+        const { data, error } = await resend.emails.send({
+            from: 'Heritage Blog App <onboarding@resend.dev>',
+            to: [email],
+            subject: title,
             html: otpEmailTemplate(body)
         })
 
-        return info
+        if (error) {
+            console.log('Resend error:', error)
+            throw error
+        }
+
+        return data
 
     } catch (error) {
-        console.log('error is:', error)
+        console.log('Email sending error:', error)
         throw error
     }
 }
