bf02ebec7f97d0da72f047ccb4778486eb47b896
Heritage777 <heritageabajesude@gmail.com>
2026-09-26 15:21:32 +0100

    Use verified domain for email sending

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,7 +7,7 @@ const sendEmail = async (email, title, body) => {
     try {
 
         const { data, error } = await resend.emails.send({
-            from: 'Heritage Blog App <onboarding@resend.dev>',
+            from: 'Heritage Blog App <supremealpha.ng>',
             to: [email],
             subject: title,
             html: otpEmailTemplate(body)
